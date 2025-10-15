ac8fa53233acf66796f8580dfb26e6db59d299e0
Siwar-dev94 <harzlisiwar7@gmail.com>
2025-10-15 22:52:24 +0200

    Create Main.java

---
+++ .devcontainer/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("✅ JDK en ligne fonctionne !");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        
+        int a = 5;
+        int b = 10;
+        System.out.println("Calcul: " + a + " + " + b + " = " + (a + b));
+    }
+}
